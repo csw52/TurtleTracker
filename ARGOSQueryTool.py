bc0b8db72bdcefef1b2a56a416293728069ad0d4
Colyer W <43612364+csw52@users.noreply.github.com>
2018-10-02 14:46:07 -0400

    Handling errors

--- ARGOSQueryTool.py
+++ ARGOSQueryTool.py
@@ -21,48 +21,54 @@ fileObj.close()
 # Create empty dictionaries
 dateDict = {}
 locationDict = {}
+try:
+    # Use a for loop to read each line, one at a time, until the list is exhausted
+    for lineString in lineStrings:
 
-# Use a for loop to read each line, one at a time, until the list is exhausted
-for lineString in lineStrings:
+        # Use the split command to parse the items in lineString into a list object
+        lineData = lineString.split("\t")
 
-    # Use the split command to parse the items in lineString into a list object
-    lineData = lineString.split("\t")
+        # Assign variables to specfic items in the list
+        recordID = lineData[0]              # ARGOS tracking record ID
+        obsDateTime = lineData[2]           # Observation date and time (combined)
+        obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
+        obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
+        obsLC = lineData[3]                 # Observation Location Class
+        obsLat = lineData[5]                # Observation Latitude
+        obsLon = lineData[6]                # Observation Longitude
 
-    # Assign variables to specfic items in the list
-    recordID = lineData[0]              # ARGOS tracking record ID
-    obsDateTime = lineData[2]           # Observation date and time (combined)
-    obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
-    obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
-    obsLC = lineData[3]                 # Observation Location Class
-    obsLat = lineData[5]                # Observation Latitude
-    obsLon = lineData[6]                # Observation Longitude
+        #Filter records that get added to the dictionary
+        if obsLC in ("1","2","3"):
 
-    #Filter records that get added to the dictionary
-    if obsLC in ("1","2","3"):
+            # Add values to dictionary
+            line44ddateDict[recordID] = obsDate
+            locationDict[recordID] = (obsLat, obsLon) 
 
-        # Add values to dictionary
-        dateDict[recordID] = obsDate
-        locationDict[recordID] = (obsLat, obsLon) 
+    # Indicate script is complete
+    print ("Finished")
 
-# Indicate script is complete
-print ("Finished")
+    # Ask user for a date, specifying the format
+    userDate = input("Enter a date (M/D/YYYY)") # input ("Enter a date (M/D/YYYY)")
 
-# Ask user for a date, specifying the format
-userDate = input("Enter a date (M/D/YYYY)") # input ("Enter a date (M/D/YYYY)")
+    # check the date
+    if not "/" in userDate:
+        print("wrong format")
 
-#create an empty list
-keyList = []
+    #create an empty list
+    keyList = []
 
-# Loop through all key, value pairs in the dateDictionary
-for k, v in dateDict.items():
-    #see if the date (the value) matches the user date
-    if v == userDate:
-        keyList.append(k)
+    # Loop through all key, value pairs in the dateDictionary
+    for k, v in dateDict.items():
+        #see if the date (the value) matches the user date
+        if v == userDate:
+            keyList.append(k)
 
-# Loop through each key and report the associated date location
-for k in keyList:
-    theDate = dateDict[k]
-    theLocation = locationDict[k]
-    theLat = theLocation[0]
-    theLon = theLocation[1]
-    print("Record {0}: Sara was see at {1}N-{2}W, on {3}".format(k,theLat,theLon,theDate))
+    # Loop through each key and report the associated date location
+    for k in keyList:
+        theDate = dateDict[k]
+        theLocation = locationDict[k]
+        theLat = theLocation[0]
+        theLon = theLocation[1]
+        print("Record {0}: Sara was see at {1}N-{2}W, on {3}".format(k,theLat,theLon,theDate))
+except Exception as e:
+    print(e)
